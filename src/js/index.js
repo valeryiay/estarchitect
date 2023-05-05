0e42f95b69983f8b24e2cffb4acc2969e9d29697
Maks Shevchenko <shevchenko@alarstudios.com>
2023-05-05 15:45:00 +0200

    feat: use dark header

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+const isFirstLoad = !sessionStorage.getItem("firsLoad");
 window.scrollTo(0, 0);
 
 window.addEventListener("DOMContentLoaded", function () {
@@ -9,7 +10,7 @@ window.addEventListener("DOMContentLoaded", function () {
     const header = document.getElementById("header");
     const service = document.getElementById("service");
     const throttled = throttle(() => {
-        header && service && header.classList.toggle("dark", service.getBoundingClientRect().top < header.style.height);
+        header && service && header.classList.toggle("header--blur", service.getBoundingClientRect().top < header.style.height);
     }, 100);
     window.addEventListener("scroll", throttled);
 
@@ -18,8 +19,8 @@ window.addEventListener("DOMContentLoaded", function () {
         centerPadding: "100px",
         slidesToShow: 1,
         slidesToScroll: 1,
-        // autoplay: true,
-        autoplaySpeed: 1000,
+        autoplay: true,
+        autoplaySpeed: 2000,
         infinite: true,
         dots: false,
         nextArrow: $(".portfolio__btn-right"),
@@ -35,6 +36,10 @@ function initAnimation() {
     const bgSlider = document.querySelector(".section-hero__bg-slider");
     bgSlider && (bgSlider.style.backgroundImage = `url(${bgSlider.dataset.src})`);
 
-    document.querySelectorAll('.hero-animate')
-        .forEach(el => setTimeout(() => el.classList.add('animated'),3000));
+    if (container && isFirstLoad) {
+        document.querySelectorAll('.hero-animate')
+            .forEach(el => setTimeout(() => el.classList.add('animated'),3000));
+        sessionStorage.setItem("firsLoad", "true");
+    }
+
 }
